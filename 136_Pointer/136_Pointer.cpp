11bbe80dec44b28654626036a25ff62808e06115
novitasalsabila <novnovv10@gmail.com>
2023-05-08 11:06:30 +0700

    Menggunakan pointer deference

--- 136_Pointer/136_Pointer.cpp
+++ 136_Pointer/136_Pointer.cpp
@@ -16,4 +16,9 @@ int main() {
 	mahasiswa& refMhs = mhs; //Pointer reference refMhs
 	refMhs.nim = 2; //Member access operator
 	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //Pointer deference pMhs
+	pMhs->nim = 3; //Arrow operator
+	mhs.showNim();
+	return 0;
 }
